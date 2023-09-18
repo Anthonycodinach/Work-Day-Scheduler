4766f707a0f98833a9e556515adf4d545f061740
Anthony Codinach <ant@imac.lan>
2023-09-18 12:59:43 -0400

    Wrapped code that interacts with the DOM and wrote functionality to handle current time changes

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+var dayjs = dayjs();
+$(document).ready(function () {
+
+var currentTime = dayjs.format("dddd, MMMM DD, YYYY");
+$("#currentDay").text(currentTime);
+var currentHour = dayjs.hour();
+var saveNote = $(".saveBtn")
+
+var timeBlock = $(".description");
+timeBlock.each(function () {
+  var timeBlockId = $(this).attr("id");
+
+  if (currentHour > timeBlockId) {
+    $(this.addClass("past"))
+  }
+  else if (currentHour === parseInt(timeBlockId)) {
+    $(this).removeClass("past");
+    $(this).addClass("present");
+  }
+  else {
+    $(this).removeClass("past")
+    $(this).removeClass("present")
+    $(this).removeClass("future")
+  }
+});
+
+})
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +48,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
