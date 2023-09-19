4f496064bb56d86a3b516bba5b4eb6ec07fce5a2
Anthony Codinach <ant@imac.lan>
2023-09-18 22:23:00 -0400

    added save text functionality & updated html to handle time chnages by id

--- Assets/script.js
+++ Assets/script.js
@@ -9,6 +9,7 @@ $("#currentDay").text(currentTime);
 var currentHour = dayjs.hour();
 var saveNote = $(".saveBtn")
 
+// Add code to apply the past, present, or future class to each time block by comparing the id to the current hour.
 var timeBlock = $(".description");
 timeBlock.each(function () {
   var timeBlockId = $(this).attr("id");
@@ -26,23 +27,20 @@ timeBlock.each(function () {
     $(this).removeClass("future")
   }
 });
+saveNote.on("click", function(){
+  var id = $(this).attr("id")
+  var dailyTask = $(this).siblings(".description").val()
+  localStorage.setItem(id, dailyTask)
+  renderTask()
+});
 
-})
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+// Function call
+renderTask()
+});
+
+function renderTask() {
+  for (var i = 9; i < 18; i++) {
+var foundTask = localStorage.getItem(i)
+$(`#${i}`).text(foundTask)
+  }
+};
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Work-Day-Scheduler
+
+## Description
 This is a simple calendar application that allows a user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+## Screenshot
+
+
+## Deployed Link
